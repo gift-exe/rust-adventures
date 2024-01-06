7d6afa2a88dafc3405ba953969bda6c6e2832ca4
Gift-py <giftabumere247@gmail.com>
2024-01-06 19:34:52 +0100

    learning how to cook beans

---
+++ first_mile/target/debug/deps/first_mile-6fee862caf5c00f5.d
@@ -0,0 +1,5 @@
+/home/g1f7/Documents/GitHub/rust-adventures/first_mile/target/debug/deps/libfirst_mile-6fee862caf5c00f5.rmeta: src/main.rs
+
+/home/g1f7/Documents/GitHub/rust-adventures/first_mile/target/debug/deps/first_mile-6fee862caf5c00f5.d: src/main.rs
+
+src/main.rs:
---
+++ first_mile/target/debug/deps/first_mile-927b29b0b2507baa.d
@@ -0,0 +1,5 @@
+/home/g1f7/Documents/GitHub/rust-adventures/first_mile/target/debug/deps/libfirst_mile-927b29b0b2507baa.rmeta: src/main.rs
+
+/home/g1f7/Documents/GitHub/rust-adventures/first_mile/target/debug/deps/first_mile-927b29b0b2507baa.d: src/main.rs
+
+src/main.rs:
